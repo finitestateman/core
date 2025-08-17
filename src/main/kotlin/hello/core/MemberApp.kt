47344a7156305c3f78bae129eb36d5185297f0fe
Finite Stateman <finitestateman@gmail.com>
2025-08-17 15:30:44 +0900

    013. 회원 도메인과 실행 테스트

---
+++ src/main/kotlin/hello/core/MemberApp.kt
@@ -0,0 +1,16 @@
+package hello.core
+
+import hello.core.member.Grade
+import hello.core.member.Member
+import hello.core.member.MemberServiceImpl
+
+fun main() {
+
+    val memberService = MemberServiceImpl()
+    val member = Member(1L, "memberA", Grade.VIP)
+    memberService.join(member)
+
+    val findMember = memberService.findMember(1L)
+    println("new member = ${member.name}")
+    println("find member = ${findMember?.name}")
+}
