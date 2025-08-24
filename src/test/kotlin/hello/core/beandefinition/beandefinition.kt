a07a9a033802624a09630ec48e8e6094f53ce434
Finite Stateman <finitestateman@gmail.com>
2025-08-24 23:17:27 +0900

    033. 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ src/test/kotlin/hello/core/beandefinition/beandefinition.kt
@@ -0,0 +1,28 @@
+package hello.core.beandefinition
+
+import hello.core.AppConfig
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+class BeandefinitionTest {
+
+    val ac = AnnotationConfigApplicationContext(AppConfig::class.java)
+    // val ac = GenericXmlApplicationContext("appConfig.xml")
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    fun findApplicationBean() {
+
+        val beanDefinitionNames = ac.beanDefinitionNames
+
+        for (beanDefinitionName in beanDefinitionNames) {
+            val beanDefinition = ac.getBeanDefinition(beanDefinitionName)
+
+            if (beanDefinition.role == BeanDefinition.ROLE_APPLICATION) {
+                println("beanDefinitionName = $beanDefinitionName beanDefinition = $beanDefinition")
+            }
+        }
+    }
+}
