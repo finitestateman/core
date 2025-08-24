b3b5d9113d5fb74a0536c4ec66336d495bd1421a
Finite Stateman <finitestateman@gmail.com>
2025-08-25 00:16:03 +0900

    036. 싱글톤 컨테이너

--- src/test/kotlin/hello/core/singleton/SingletonTest.kt
+++ src/test/kotlin/hello/core/singleton/SingletonTest.kt
@@ -1,9 +1,12 @@
 package hello.core.singleton
 
 import hello.core.AppConfig
+import hello.core.member.MemberService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.getBean
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 class SingletonTest {
 
@@ -48,6 +51,22 @@ class SingletonTest {
 
         assertThat(singletonService1).isSameAs(singletonService2)
         assertThat(singletonObject1).isSameAs(singletonObject2)
+
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    fun springContainer() {
+
+        val ac = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+        val memberService1 = ac.getBean<MemberService>("memberService")
+        val memberService2 = ac.getBean<MemberService>("memberService")
+
+        println("memberService1 = $memberService1")
+        println("memberService2 = $memberService2")
+
+        assertThat(memberService1).isSameAs(memberService2)
 
     }
 }
