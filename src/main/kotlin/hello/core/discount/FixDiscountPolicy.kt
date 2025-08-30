8910f323b2d18e6c50bc50776850828fbf7a79c5
Finite Stateman <finitestateman@gmail.com>
2025-08-30 19:59:13 +0900

    048. 조회 빈이 2개 이상 - 문제

--- src/main/kotlin/hello/core/discount/FixDiscountPolicy.kt
+++ src/main/kotlin/hello/core/discount/FixDiscountPolicy.kt
@@ -2,7 +2,9 @@ package hello.core.discount
 
 import hello.core.member.Grade
 import hello.core.member.Member
+import org.springframework.stereotype.Component
 
+@Component
 class FixDiscountPolicy(private val discountFixAmount: Int = 1_000) : DiscountPolicy {
 
     override fun discount(member: Member, price: Int) =
