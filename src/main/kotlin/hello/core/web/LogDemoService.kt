8eff412614e00ec707f15bcf475d78497d3725be
Finite Stateman <finitestateman@gmail.com>
2025-08-31 03:51:48 +0900

    064. 스코프와 프록시

--- src/main/kotlin/hello/core/web/LogDemoService.kt
+++ src/main/kotlin/hello/core/web/LogDemoService.kt
@@ -1,12 +1,11 @@
 package hello.core.web
 
 import hello.core.common.MyLogger
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Service
 
 @Service
 class LogDemoService(
-    private val myLoggerProvider: ObjectProvider<MyLogger>
+    private val myLogger: MyLogger
 ) {
 
     fun logic(id: String) {
